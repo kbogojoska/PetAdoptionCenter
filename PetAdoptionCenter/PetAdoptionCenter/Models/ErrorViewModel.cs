a701f13e7917becd468b8fa153a4c8c65c0ab97b
kbogojoska <107806557+kbogojoska@users.noreply.github.com>
2024-10-25 17:24:28 +0200

    initial

---
+++ PetAdoptionCenter/PetAdoptionCenter/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PetAdoptionCenter.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
